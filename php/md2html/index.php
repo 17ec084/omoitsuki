e70d1e057d4855f8a8f37e64b27c183d687c63f8
Tomotaka_Hirata <17ec084@ms.dendai.ac.jp>
2019-04-18 07:32:21 +0900

    h1~6だけ変換できるようになったはず

--- php/md2html/index.php
+++ php/md2html/index.php
@@ -6,50 +6,20 @@ if(!isset($_GET['url']) || ($str=curl_get_contents($_GET['url']))===false)
     return;
 }
 
-$newLine="\\n|\\r|(\\r\\n)";
-$exceptNewLine="^(".$newLine.")";
+$beginLine="^";
+$endLine="$";
+$exceptNewLine="^(".$endLine.")";
+$d="~";//デリミタ
 
-$pattern=$newLine."######(".$exceptNewLine."*)".$newLine."/u";
-$replace='<h6>$2</h6>';
-$str=preg_replace($pattern, $replace, $str);
-/*
-正規表現「\\n#######(^\\r)*\\n」を、
-「<h7>(^\\n)*</h7>」に置き換える
-* /
-$str=
-/*
-上記を、h6～h1について、この順で実行
-* /
+$conf=array($beginLine, $endLine, $exceptNewLine, $d);
+//h6to1等々の関数に渡すとき、冗長化を防ぐ
+//本来はグローバル変数にすればいい話だが、lcrp関数にしたくなった時のことを考えると、そうしたくない。
 
-$str=
-/*
-正規表現「<!\\-\\- hole(^(\\-\\->))*\\-\\->」を、
-「<input type="button" value="?" onClick="alert("(中身)");">」に置き換える
-* /
 
-$str=
-/*
-正規表現「\\n\\-\\-\\-\\-」を、
-「<hr>」に置き換える
-* /
+$str=h6to1($str,$conf);
+//「#」をh6～1に変換
+//先にh1を置き換えようとするとh2～h6が誤検出される。パターンの冗長化を防ぐため、あえてこの問題を解決していない。
 
-$str=
-/*
-正規表現「!\\[(^])*]( |\\t|\\n)*\\((^(\\))*\\)」を、
-「<img src="**">」に置き換える
-* /
-
-$str=
-/*
-正規表現「\\[(^])*]( |\\t|\\n)*\\((^(\\))*\\)」を、
-「<a href="**">**</a>」に置き換える
-* /
-
-$str=
-/*
-正規表現「<!\\-\\- lc msg=(^\\-)*-->」を、
-(msgの値)に置き換える
-*/
 
 $str='<html>
     <head>
@@ -64,9 +34,44 @@ $str='<html>
         <a href="https://www.change.org/p/%E5%A5%B3%E6%80%A7%E5%B0%82%E7%94%A8%E8%BB%8A%E4%B8%A1%E3%82%92%E5%B0%8E%E5%85%A5%E3%81%97%E3%81%A6%E3%81%84%E3%82%8B%E9%89%84%E9%81%93%E4%BC%9A%E7%A4%BE-%E7%94%B7%E6%80%A7%E5%B0%82%E7%94%A8%E8%BB%8A%E4%B8%A1%E3%81%AE%E5%B0%8E%E5%85%A5%E3%82%92%E6%B1%82%E3%82%81%E3%81%BE%E3%81%99">男性専用車両導入を求める署名運動</a>もしておりますので、ご協力ください。<br>
         (一部サービスは、こちらの署名への協力を条件に提供しております。ご了承ください)
         <hr>
-".$str."
+'.$str.'
     </body>
-</html>";
+</html>';
 
 print $str;
 
+
+
+    function curl_get_contents( $url, $timeout = 60 )
+    {
+    //外部サイトから引用
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_HEADER, false );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
+        $result = curl_exec( $ch );
+        curl_close( $ch );
+        return $result;
+    }
+
+    function h6to1($str,$conf)
+    {
+        $beginLine=$conf[0];
+        $endLine=$conf[1];
+        $exceptNewLine=$conf[2];
+        $d=$conf[3];//デリミタ
+
+        for($i=6; $i>1-1; $i--)
+        {
+            $sharp="";
+            for($j=0; $j<$i; $j++)
+            {
+                $sharp.="#";
+            }
+            $pattern=$d.$beginLine.$sharp."(.*)".$endLine.$d."u";
+            $replace='<h'.$i.'>$1</h'.$i.'>';
+            $str=preg_replace($pattern, $replace, $str);
+        }
+        return $str;
+    }
--- php/md2html/sample.md
+++ php/md2html/sample.md
@@ -1,1 +1,7 @@
 ###### this line should be h6
+
+# this line should be h1
+
+#### this line should be h4
+
+
